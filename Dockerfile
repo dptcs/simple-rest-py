b182bdd23e09260ed6cddf7df96bb4a556e8aa0a
deepak <deepak.sisupalan@mnscorp.net>
2022-10-08 13:13:18 +0100

    chore: format files

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-#We will use nginx lateste as base 
+#We will use nginx lateste as base
 FROM nginx:latest
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update -q && apt-get install -qqy \
-nano python3-pip python3-dev nginx gunicorn  systemd
+    nano python3-pip python3-dev nginx gunicorn systemd
 
 ADD . /myflaskproject
 WORKDIR /myflaskproject
--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def helloWorld():
-   if request.method == 'GET':
-      return jsonify({"World": "HELLO !!"})
+    if request.method == 'GET':
+        return jsonify({"World": "HELLO !!"})
+
+
 if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
