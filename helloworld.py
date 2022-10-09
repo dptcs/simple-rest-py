612fee9caf07f55b289c88adcc5135afeb426b5a
deepak <deepak.sisupalan@mnscorp.net>
2022-10-09 11:00:35 +0100

    chore:  gha app deploy

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def helloWorld():
     if request.method == 'GET':
-        return jsonify({"World": "HELLO !!"})
+        return jsonify({"World": "HELLOZ !!"})
 
 
 if __name__ == "__main__":
