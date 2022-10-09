7dbd519f98aea9293b113d6fe9218c16665b5dd2
deepak <deepak.sisupalan@mnscorp.net>
2022-10-09 13:47:18 +0100

    test: trigger

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def helloWorld():
     if request.method == 'GET':
-        return jsonify({"New": "York"})
+        return jsonify({"New": "Delhi"})
 
 
 if __name__ == "__main__":
