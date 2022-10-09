1bd0cf08a6f54bfc299783035cf32b5267499172
deepak <deepak.sisupalan@mnscorp.net>
2022-10-09 11:03:01 +0100

    chore: app deploy test

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def helloWorld():
     if request.method == 'GET':
-        return jsonify({"World": "HELLOZ !!"})
+        return jsonify({"World": "HELLOZZ !!"})
 
 
 if __name__ == "__main__":
