966fd8764fd23f8645d2487d7837d7fb16fdccfb
deepak <deepak.sisupalan@mnscorp.net>
2022-10-09 09:36:18 +0100

    chore: add docker init

--- infra/terraform-vm/main.tf
+++ infra/terraform-vm/main.tf
@@ -1,5 +1,4 @@
 module "vm" {
   source                     = "git::https://github.com/dptcs/terraform-azure-vm.git?ref=v0.0.1"
   resource_group_name_prefix = var.resource_group_name_prefix
-  scfile                     = ".terraform/modules/vm/docker_init.sh"
 }
